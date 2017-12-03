f4a92628f48d945d17a750392a1676f32906c6b4
Keisuke Kido <kskkido@gmail.com>
2017-12-04 02:26:59 +0900

    add extension

--- src/server/index.js
+++ src/server/index.js
@@ -13,7 +13,7 @@ export default app
 	.use(bodyParser.urlencoded({ extended: false }))
 	.use(bodyParser.json())
 
-	.use('/dist', express.static(PATH_STATIC))
+	.use('/dist', express.static(PATH_STATIC, { extensions: ['js'] }))
 
 	.get('/', (req, res) => {
 		res.redirect('/issues?page=1')
